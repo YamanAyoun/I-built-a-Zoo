c45d46b199e75e8f8dd25c48927af5b67176210b
YamanAyoun <yamanayoun0@gmail.com>
2023-07-01 18:43:29 +0300

    Add project files.

---
+++ I built a Zoo!/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace I_built_a_Zoo_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
