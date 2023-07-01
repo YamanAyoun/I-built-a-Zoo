106645d9224246543c22fd2892f816dd88bbb902
YamanAyoun <yamanayoun0@gmail.com>
2023-07-01 22:14:00 +0300

    update

---
+++ TestZoo/UnitTest1.cs
@@ -0,0 +1,38 @@
+using I_built_a_Zoo_.Classes.Animal.FlyingAnimal;
+using I_built_a_Zoo_.Classes.Animal.MammalAnimal;
+using I_built_a_Zoo_.Classes.Animal.MarineAnimal;
+using I_built_a_Zoo_.Classes.Animal.PetAnimal;
+
+namespace TestZoo
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMammalAnimal()
+        {
+            Mammal lion = new Lion();
+            Assert.Equal("Lion Eating Meat", lion.Eat());
+        }
+
+        [Fact]
+        public void TestPetAnimal()
+        {
+            Pet cat = new Cat();
+            Assert.Equal("Cat Sound: Meow", cat.Sound());
+        }
+
+        [Fact]
+        public void TestFlyingAnimal()
+        {
+            Flying chicken = new Chicken();
+            Assert.Equal("Chicken is Sleeping", chicken.Sleep());
+        }
+
+        [Fact]
+        public void TestMarineAnimal()
+        {
+            Marine fish = new Fish();
+            Assert.Equal("Fish is Eating", fish.Eat());
+        }
+    }
+}
